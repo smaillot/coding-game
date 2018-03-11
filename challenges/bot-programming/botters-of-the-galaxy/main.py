b121f48a8f8f083faa9bafa3c77bef3854f6b5ec
Stéphane Maillot <s.maillot2@gmail.com>
2018-03-11 17:24:00 +0100

    first version

--- challenges/bot-programming/botters-of-the-galaxy/main.py
+++ challenges/bot-programming/botters-of-the-galaxy/main.py
@@ -1,6 +1,7 @@
 import sys
 import math
 import random
+import numpy as np
 
 HEROES = ['DEADPOOL', 'DOCTOR_STRANGE', 'HULK', 'IRONMAN', 'VALKYRIE']
 
@@ -9,8 +10,6 @@ def debug(msg):
 
 class Game(object):
     def __init__(self):
-        # debug
-        debug('read my_team')
         self.my_team = int(input())
         self.players = [Player(0), Player(1)]
         self.map = Map()
@@ -19,39 +18,30 @@ class Game(object):
         self.read_objects()
         self.turn = 0
         
+    def __getattr__(self, key):
+        
+        if key == 'me':
+            return self.players[self.my_team]
+        elif key == 'ennemy':
+            return self.players[1 - self.my_team]
+        
     def read_spawns(self):
-        # debug
-        debug('read n_spawns')
         self.map.read_spawns(int(input()))        
         
     def read_objects(self):
-        # debug
-        debug('read n_objects')
         for i in range(int(input())):
-            # debug
-            debug('read object line')
             self.objects.append(Object(*input().split()))
 
     def read_entities(self):
         self.players[self.my_team].reset_entities()
         self.players[1 - self.my_team].reset_entities()
-        # debug
-        debug('read n_entities')
         for i in range(int(input())):
-            # debug
-            debug('read entity line')
             entity = input().split()
             self.players[int(entity[1])].add_entity(*entity)
 
     def read_turn(self):
-        # debug
-        debug('read my gold')
         self.players[self.my_team].gold = int(input())
-        # debug
-        debug('read ennemy gold')
         self.players[1 - self.my_team].gold = int(input())
-        # debug
-        debug('read heroes waiting')
         heroes_waiting = int(input())
         if heroes_waiting < 0:
             return 0
@@ -75,6 +65,16 @@ class Player(object):
             self.heroes.append(Hero(*args))
         else:
             self.entities.append(Entity(*args[:13]))
+            
+    def get_bounds(self, unit_type):
+        if len(self.entities) > 0:
+            x_axis = [e.x for e in self.entities if e.type.upper() == unit_type.upper()]
+            if len(x_axis) > 0:
+                return [np.min(x_axis), np.max(x_axis)]
+            else:
+                return []
+        else:
+            return []
         
 class Map(object):
     def __init__(self):
@@ -112,32 +112,35 @@ class Object(object):
 
 class Entity(object):
     def __init__(self, id, team, entity_type, x, y, attackRange, health, max_health, shield, attack_damage, movement_speed, stun_duration, gold_value):
-        self.id = id
-        self.team = team
+        self.id = int(id)
+        self.team = int(team)
         self.type = entity_type
-        self.x = x
-        self.y = y
-        self.attackRange = attackRange
-        self.health = health
-        self.maxHealth = max_health
-        self.shield = shield
-        self.attack_damage = attack_damage
-        self.movement_speed = movement_speed
-        self.stun_duration = stun_duration
-        self.cost = gold_value
+        self.x = int(x)
+        self.y = int(y)
+        self.attackRange = int(attackRange)
+        self.health = int(health)
+        self.maxHealth = int(max_health)
+        self.shield = int(shield)
+        self.attack_damage = int(attack_damage)
+        self.movement_speed = int(movement_speed)
+        self.stun_duration = int(stun_duration)
+        self.cost = int(gold_value)
+        
+    def distance(self, entity):
+        return np.sqrt(pow(self.x - entity.x, 2) + pow(self.y - entity.y, 2))
         
 class Hero(Entity):
     def __init__(self, id, team, entity_type, x, y, attackRange, health, max_health, shield, attack_damage, movement_speed, stun_duration, gold_value, count_down_1, count_down_2, count_down_3, mana, max_mana, mana_regeneration, hero_type, is_visible, items_owned):
         Entity.__init__(self, id, team, entity_type, x, y, attackRange, health, max_health, shield, attack_damage, movement_speed, stun_duration, gold_value)
-        self.count_down_1 = count_down_1
-        self.count_down_2 = count_down_2
-        self.count_down_3 = count_down_3
-        self.mana = mana
-        self.max_mana = max_mana
-        self.mana_regeneration = mana_regeneration
+        self.count_down_1 = int(count_down_1)
+        self.count_down_2 = int(count_down_2)
+        self.count_down_3 = int(count_down_3)
+        self.mana = int(mana)
+        self.max_mana = int(max_mana)
+        self.mana_regeneration = int(mana_regeneration)
         self.hero_type = hero_type
-        self.is_visible = is_visible
-        self.items_owned = items_owned
+        self.is_visible = int(is_visible)
+        self.items_owned = int(items_owned)
 
 class Printer(object):
     def hero(self, name):
@@ -145,15 +148,18 @@ class Printer(object):
     
     def wait(self):
         print('WAIT')
+        
+    def move(self, x, y):
+        print('MOVE {} {}'.format(x, y))
 
     def attack(self, unit_id):
         print('ATTACK {}'.format(unit_id))
 
-    def attack_nearest(self):
-        print('ATTACK_NEAREST')
+    def attack_nearest(self, unit_type):
+        print('ATTACK_NEAREST {}'.format(unit_type))
 
     def attack_move(self, x, y, unit_id):
-        print('MOVE_ATTACK {} {} {}')
+        print('MOVE_ATTACK {} {} {}'.format(x, y, unit_id))
 
     def buy(self, item_name):
         print('BUY {}'.format(item_name))
@@ -172,7 +178,32 @@ solver = Solver()
 while True:
     round_type = game.read_turn() # if the turn is frozen, choose a hero
     game.read_entities()
-    if round_type < 0:
+    if round_type == 0:
         printer.hero(solver.choose_hero())
     else: # regular turn
-        printer.wait()
+        #if len(game.me.heroes) > 0:
+        #    if len(game.ennemy.heroes) > 0:
+        #        # printer.attack_move(game.ennemy.heroes[0].x, game.ennemy.heroes[0].y, game.ennemy.heroes[0].id)
+        #        printer.attack_nearest('HERO ')
+        #    else: 
+        #        printer.attack_nearest('UNIT ')
+        #else:
+        #    printer.wait()
+        if len(game.me.heroes) > 0:
+            bounds = game.me.get_bounds('UNIT')
+            
+            can_attack = False
+            for e in game.ennemy.entities:
+                if e.distance(game.me.heroes[0]) < game.me.heroes[0].attackRange:
+                    can_attack = True
+                    break
+            
+            if len(game.me.heroes) > 0 and can_attack:
+                printer.attack_nearest('UNIT')
+            else:
+                if len(bounds) > 0: 
+                    printer.move(bounds[1 - game.my_team], game.me.heroes[0].y)
+                else:
+                    printer.wait()
+        else:
+            printer.wait()
